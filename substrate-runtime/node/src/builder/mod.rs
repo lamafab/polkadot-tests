4fe68deccfa587aaa348dbb9db31c22d67668f73
lamafab <42901763+lamafab@users.noreply.github.com>
2020-11-01 18:09:17 +0100

    implement TryFrom<TxtHeader> for Header

--- substrate-runtime/node/src/builder/mod.rs
+++ substrate-runtime/node/src/builder/mod.rs
@@ -2,6 +2,33 @@ pub mod import_block;
 
 use node_template_runtime::{Signature, Address, BlockNumber, Header, Block, UncheckedExtrinsic, SignedExtra};
 
+/*
+Summary:
+
+/// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
+pub type Signature = MultiSignature;
+/// The address format for describing accounts.
+pub type Address = AccountId;
+/// An index to a block.
+pub type BlockNumber = u32;
+/// Block header type as expected by this runtime.
+pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+/// Block type as expected by this runtime.
+pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+/// Unchecked extrinsic type as expected by this runtime.
+pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
+/// The SignedExtension to the basic transaction logic.
+pub type SignedExtra = (
+	frame_system::CheckSpecVersion<Runtime>,
+	frame_system::CheckTxVersion<Runtime>,
+	frame_system::CheckGenesis<Runtime>,
+	frame_system::CheckEra<Runtime>,
+	frame_system::CheckNonce<Runtime>,
+	frame_system::CheckWeight<Runtime>,
+	pallet_transaction_payment::ChargeTransactionPayment<Runtime>
+);
+*/
+
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TestLayout<T> {
