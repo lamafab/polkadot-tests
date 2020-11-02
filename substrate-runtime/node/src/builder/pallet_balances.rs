2fdd0fce88175eb07ab150356c4a19846d0cb0be
lamafab <42901763+lamafab@users.noreply.github.com>
2020-11-02 01:00:50 +0100

    interpret Call as subcommand

--- substrate-runtime/node/src/builder/pallet_balances.rs
+++ substrate-runtime/node/src/builder/pallet_balances.rs
@@ -2,33 +2,26 @@ use crate::Result;
 use crate::chain_spec::get_account_id_from_seed;
 use sp_core::sr25519;
 use structopt::StructOpt;
-use std::str::FromStr;
 
 #[derive(Debug, StructOpt)]
 pub struct PalletBalancesCmd {
+    #[structopt(subcommand)]
     call: Call,
 }
 
 #[derive(Debug, StructOpt)]
 pub enum Call {
-    Transfer,
-}
-
-impl FromStr for Call {
-    type Err = failure::Error;
-
-    fn from_str(val: &str) -> Result<Self> {
-        match val {
-            "transfer" => Ok(Call::Transfer),
-            _ => Err(failure::err_msg(format!("Call to function '{}' is not supported", val))),
-        }
-    }
+    Transfer {
+        from: String,
+        to: String,
+        balance: u128,
+    },
 }
 
 impl PalletBalancesCmd {
     pub fn run(&self) -> Result<()> {
-        match self {
-            Transfer => {
+        match &self.call {
+            Call::Transfer { from, to, balance } => {
 
             }
         }
